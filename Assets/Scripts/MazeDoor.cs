9a9730a795b99036d434927b8be4455f4c815d0b
konstantinos Giannousis <kongiann81@gmail.com>
2016-01-13 21:54:57 +0100

    2md commit

---
+++ Assets/Scripts/MazeDoor.cs
@@ -0,0 +1,79 @@
+﻿using UnityEngine;
+
+public class MazeDoor : MazePassage {
+
+	private static Quaternion
+		normalRotation = Quaternion.Euler(0f, -90f, 0f),
+		mirroredRotation = Quaternion.Euler(0f, 90f, 0f);
+
+	public Transform hinge;
+
+	private bool isMirrored;
+
+	public bool isDoorOpen = false;
+
+	private MazeDoor OtherSideOfDoor {
+		get {
+			return otherCell.GetEdge(direction.GetOpposite()) as MazeDoor;
+		}
+	}
+	
+	public override void Initialize (MazeCell primary, MazeCell other, MazeDirection direction) {
+		base.Initialize(primary, other, direction);
+		if (OtherSideOfDoor != null) {
+			isMirrored = true;
+			hinge.localScale = new Vector3(-1f, 1f, 1f);
+			Vector3 p = hinge.localPosition;
+			p.x = -p.x;
+			hinge.localPosition = p;
+		}
+		for (int i = 0; i < transform.childCount; i++) {
+			Transform child = transform.GetChild(i);
+			if (child != hinge) {
+				child.GetComponent<Renderer>().material = cell.room.settings.wallMaterial;
+			}
+		}
+	}
+
+//	public override void OnPlayerEntered () {
+//		OtherSideOfDoor.hinge.localRotation = hinge.localRotation = isMirrored ? mirroredRotation : normalRotation;
+//		OtherSideOfDoor.cell.room.Show();
+//	}
+//	
+//	public override void OnPlayerExited () {
+//		OtherSideOfDoor.hinge.localRotation = hinge.localRotation = Quaternion.identity;
+//		OtherSideOfDoor.cell.room.Hide();
+//	}
+
+	public override void OnPlayerEntered (bool canOpenDoor) {
+		if (!canOpenDoor) return;
+
+		if (isDoorOpen && canOpenDoor) {
+			CloseDoor ();
+			return;
+		}
+		if (!isDoorOpen)
+			isDoorOpen = true;
+		
+		OtherSideOfDoor.isDoorOpen = true;
+
+		OtherSideOfDoor.hinge.localRotation = hinge.localRotation = isMirrored ? mirroredRotation : normalRotation;
+		OtherSideOfDoor.cell.room.Show();
+	}
+
+	private void CloseDoor() {
+		OtherSideOfDoor.hinge.localRotation = hinge.localRotation = Quaternion.identity;
+		OtherSideOfDoor.cell.room.Hide();
+	//	OnPlayerExited (true);
+		isDoorOpen = false;
+		OtherSideOfDoor.isDoorOpen = false;
+	}
+
+	public override void OnPlayerExited (bool canOpenDoor) {
+		return;
+		if (!canOpenDoor && !isDoorOpen) return;
+
+		OtherSideOfDoor.hinge.localRotation = hinge.localRotation = Quaternion.identity;
+	    OtherSideOfDoor.cell.room.Hide();
+	}
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,69 @@
+﻿using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+	private MazeCell currentCell;
+
+	private MazeDirection currentDirection;
+
+	public void SetLocation (MazeCell cell) {
+		if (currentCell != null) {
+			currentCell.OnPlayerExited(canOpenDoor);
+		}
+		currentCell = cell;
+		transform.localPosition = cell.transform.localPosition;
+		currentCell.OnPlayerEntered(canOpenDoor);
+	}
+
+	private void Move (MazeDirection direction) {
+		MazeCellEdge edge = currentCell.GetEdge(direction);
+		if (edge is MazeDoor) {
+			var door = edge as MazeDoor;
+			if (canOpenDoor || door.isDoorOpen)
+				SetLocation(edge.otherCell);
+			return;
+		}
+		if (edge is MazePassage) {
+			SetLocation(edge.otherCell);
+		}
+	}
+
+	private void Look (MazeDirection direction) {
+		transform.localRotation = direction.ToRotation();
+		currentDirection = direction;
+	}
+
+	private bool canOpenDoor;
+
+	public void OpenDoor(MazeDirection direction) {
+		canOpenDoor = true;
+		currentCell.OnPlayerEntered(canOpenDoor);
+
+	}
+
+	private void Update () {
+		if (canOpenDoor)
+			canOpenDoor = false;
+
+		if (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow)) {
+			Move(currentDirection);
+		}
+		else if (Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.RightArrow)) {
+			Move(currentDirection.GetNextClockwise());
+		}
+		else if (Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.DownArrow)) {
+			Move(currentDirection.GetOpposite());
+		}
+		else if (Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.LeftArrow)) {
+			Move(currentDirection.GetNextCounterclockwise());
+		}
+		else if (Input.GetKeyDown(KeyCode.Q)) {
+			Look(currentDirection.GetNextCounterclockwise());
+		}
+		else if (Input.GetKeyDown(KeyCode.E)) {
+			Look(currentDirection.GetNextClockwise());
+		}
+		else if (Input.GetKeyDown(KeyCode.R)) 
+			OpenDoor(currentDirection);
+	}
+}
