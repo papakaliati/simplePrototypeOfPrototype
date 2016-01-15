e96d6c998469d4d3ed5d4e26a8cf19c0f17fb212
konstantinos Giannousis <kongiann81@gmail.com>
2016-01-15 15:40:01 +0100

    Refactoring

---
+++ Assets/Scripts/MazeComplexity.cs
@@ -0,0 +1,74 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+public class MazeComplexity  {
+	
+	public Dictionary <MazeRoom, Dictionary< MazeCell, int>> AcceptableLocationsDictionary = 
+		new Dictionary<MazeRoom, Dictionary<MazeCell, int>>();
+
+	public MazeComplexity(List<MazeRoom> maze ) {
+		CalculateMazeComplexity (maze);
+	}
+
+	private Dictionary <MazeRoom, Dictionary< MazeCell, int>> roomMapping = new Dictionary<MazeRoom, Dictionary<MazeCell, int>>();
+
+	//A room is considered available if it has size more than 10 cells
+	private List<MazeRoom> GetAvailableRoomsForObjectPlacement (List<MazeRoom> rooms) {
+		return rooms.OrderByDescending (x => x.RoomSize).Where (x => x.RoomSize > 10).ToList ();
+	}
+
+	private void CalculateMazeComplexity(List<MazeRoom> rooms){ 
+		var availableRooms = GetAvailableRoomsForObjectPlacement (rooms);
+		foreach (var room in availableRooms) 
+			CalculateRoomComplexity (room);
+
+		FilterDictionaries (); 
+	}
+
+	private void FilterDictionaries (){
+		foreach (var pair in roomMapping) {
+			Debug.LogFormat (" RoomID : {0} RoomSize  : {1}", pair.Key.RoomId, pair.Key.RoomSize); 
+			foreach (var pairy in pair.Value)
+				if (pairy.Value > 1) {
+					Debug.LogFormat ("The Cell : {0} has complexity : {1}", pairy.Key.name, pairy.Value); 
+					SaveComplexityToDictionary (pairy.Key, pair.Key, pairy.Value, AcceptableLocationsDictionary); 
+				}
+		}
+	}
+
+	private void CalculateRoomComplexity(MazeRoom room) {
+		foreach (var cell in room.cells)
+			CalculateCellComplexity (cell, room);
+	} 
+
+	private void CalculateCellComplexity(MazeCell cell, MazeRoom room) {
+		var complexity = GetCellComplexity (cell, room);
+		SaveComplexityToDictionary (cell, room, complexity, roomMapping);
+	}
+
+	private void SaveComplexityToDictionary (MazeCell cell, MazeRoom room, int complexity, Dictionary <MazeRoom, Dictionary< MazeCell, int>> dictionary){
+		if (!dictionary.ContainsKey (room)) {
+			var tempDick = new Dictionary<MazeCell,int> () { { cell, 0 } };
+			dictionary.Add (room, tempDick);
+		} else if (!dictionary [room].ContainsKey (cell))
+			dictionary [room].Add (cell, complexity);
+		else
+			dictionary [room] [cell] = complexity;
+	}
+
+	private int GetCellComplexity(MazeCell cell, MazeRoom room) {
+		int complexity = 0;
+		bool edgeReached = false;
+
+		while (!edgeReached) {
+			var neighborhoodCells = MazeCell.GetNeighborhoodCells (cell, room, complexity);
+			if (neighborhoodCells.Count < 8)
+				edgeReached = true;
+			else
+				complexity++;
+		}
+		return complexity;
+	}
+}
---
+++ Assets/Scripts/PropabiliesCalulations.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+public class PropabiliesCalulations <T> {
+
+	public static int GetRandomSelection (T[] attributionRates) {
+		List<ItemType> items = new List<ItemType> ();
+		for (var i = 0; i < attributionRates.Count (); i++)
+			items.Add (new ItemType (i, attributionRates [i]));
+		return RandomSelect (items);
+	}
+
+	private static int RandomSelect (List<ItemType> cards) {
+		var deck = new List<ItemType>();
+		cards.ForEach (c => {
+			for (int i = 0; i < System.Convert.ToInt32(c.AttributionRate); i++)
+				deck.Add (c);
+		});
+		deck = deck.OrderBy(c => System.Guid.NewGuid()).ToList();
+		return deck[0].Id;
+	}
+
+	public class ItemType {
+		public int Id { private set; get;}
+		public T AttributionRate { private set; get; }
+
+		public ItemType (int Id, T AttributionRate){
+			this.Id= Id;
+			this.AttributionRate = AttributionRate;
+		}
+	}
+}
