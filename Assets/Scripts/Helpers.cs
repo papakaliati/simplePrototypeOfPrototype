aa277b983b0621e1207d0d597186000ea5acb367
konstantinos Giannousis <kongiann81@gmail.com>
2016-01-28 11:52:03 +0100

    Refactoring

--- Assets/Scripts/Helpers.cs
+++ Assets/Scripts/Helpers.cs
@@ -8,6 +8,15 @@ public class Helpers {
 
 	public const string kDeletedDoorDescription = "deleted";
 
+	/// <summary>
+	/// Adds to dictionary.
+	/// If Key already pressent, it overrides it 
+	/// </summary>
+	/// <param name="dict">Dictionary.</param>
+	/// <param name="t1">T1, Dictionary key Type.</param>
+	/// <param name="t2">T2 Dictionary value Type.</param>
+	/// <typeparam name="T1">Dictionary key.</typeparam>
+	/// <typeparam name="T2">Dictionary value.</typeparam>
 	public static void AddToDictionary<T1,T2> ( IDictionary<T1,T2> dict, T1 t1, T2 t2) {
 		if (dict.ContainsKey (t1))
 			dict [t1] = t2;
@@ -15,6 +24,14 @@ public class Helpers {
 			dict.Add (t1, t2);
 	}
 
+	/// <summary>
+	/// Extracts the type of the object of.
+	/// If more than one public variable with that name, the first will always be returned
+	/// If none found, null will be returned so a null check on the caller is required
+	/// </summary>
+	/// <returns>The object of type.</returns>
+	/// <param name="parentClass">Parent class.</param>
+	/// <typeparam name="T">The type of the variable requested.</typeparam>
 	public static T ExtractObjectOfType<T>(object parentClass) {
 		var fieldValues = parentClass.GetType()
 			.GetFields()
